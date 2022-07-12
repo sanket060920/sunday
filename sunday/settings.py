57d4f72ed1ce7606af6502a09cf2efb416e43432
sanket godse <sanketgodse059@gmail.com>
2022-07-12 13:37:41 +0530

    3336553

--- sunday/settings.py
+++ sunday/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oy)eyjmf#^%dwvmwgbv6=r_2&d*ubw=(91hnsy)*8ik1t!^aln'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','sanketgodse.herokuapp.com']
 
@@ -125,7 +123,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
 
-django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
