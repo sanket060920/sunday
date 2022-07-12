a8ffc3850b5b2c17731403dd71f1d57fe161b715
sanket060920 <102405045+sanket060920@users.noreply.github.com>
2022-07-12 11:49:31 +0530

    Add files via upload

---
+++ sunday/urls.py
@@ -0,0 +1,32 @@
+"""sunday URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from http import server
+from django import urls
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+
+from django.views.static import serve
+from django.conf.urls.static import url
+
+
+urlpatterns = [
+    path('',include('jan.urls')),
+    path('admin/', admin.site.urls),
+    
+    urls(r'^media/(?P<path>.*)$', server,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+]
