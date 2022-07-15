06d82c8621411b11c793f659c6943158692368cb
sanket godse <sanketgodse06@gmail.com>
2022-07-15 07:12:02 +0530

    1111

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -4,8 +4,8 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
 }
 
 // Close navbar when link is clicked
@@ -14,21 +14,21 @@ const navLink = document.querySelectorAll(".nav-link");
 navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
 }
 
 // Event Listeners: Handling toggle event
 const toggleSwitch = document.querySelector(
-  '.theme-switch input[type="checkbox"]'
+    '.theme-switch input[type="checkbox"]'
 );
 
 function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-  }
+    if (e.target.checked) {
+        document.documentElement.setAttribute("data-theme", "light");
+    } else {
+        document.documentElement.setAttribute("data-theme", "dark");
+    }
 }
 
 toggleSwitch.addEventListener("change", switchTheme, false);
@@ -36,27 +36,27 @@ toggleSwitch.addEventListener("change", switchTheme, false);
 //  Store color theme for future visits
 
 function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark"); //add this
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light"); //add this
-  }
+    if (e.target.checked) {
+        document.documentElement.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark"); //add this
+    } else {
+        document.documentElement.setAttribute("data-theme", "light");
+        localStorage.setItem("theme", "light"); //add this
+    }
 }
 
 // Save user preference on load
 
-const currentTheme = localStorage.getItem("theme")
-  ? localStorage.getItem("theme")
-  : null;
+const currentTheme = localStorage.getItem("theme") ?
+    localStorage.getItem("theme") :
+    null;
 
 if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme);
+    document.documentElement.setAttribute("data-theme", currentTheme);
 
-  if (currentTheme === "dark") {
-    toggleSwitch.checked = true;
-  }
+    if (currentTheme === "dark") {
+        toggleSwitch.checked = true;
+    }
 }
 
 //Adding date
