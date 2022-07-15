aaa54bfde2944bae322c69d5854fba7339d4fb20
sanket godse <sanketgodse06@gmail.com>
2022-07-15 07:00:27 +0530

    1111

--- sunday/settings.py
+++ sunday/settings.py
@@ -121,6 +121,9 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
 
 # Default primary key field type
