82b9274569ae076d15950828852726c11f3e6939
sanket godse <sanketgodse06@gmail.com>
2022-07-15 07:04:45 +0530

    1111

--- sunday/settings.py
+++ sunday/settings.py
@@ -121,8 +121,6 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
 
